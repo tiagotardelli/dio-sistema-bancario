9fcb6c099a6b75b505e383be390afef062dbd509
Tiago Tardelli <tiagob.tardelli@gmail.com>
2024-05-30 22:44:22 -0300

    Programa Desenvolvido

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # dio-sistema-bancario
 Sitema bancário para entrega do projeto de desafio
+
+# Escopo
+Criar um sistema bancário com as operações: sacar, depositar e visualizar extrato
+##
+### Operação Depósito:
+Deve ser possível depositar valores positivos para conta bancária. A v1 do projeto deve trabalhar apenas com 1 usuário, dessa forma não é necessário se preocupar com identificação do número da agência e conta bancária. Todos os depósitos devem ser armazenados em uma variável e exibidos na operação de extrato.
+### Operação de Saque:
+O sistema deve permitir realizar 3 saques diários com limite máximo de R$500,00 por saque. Caso o usuário não tenha saldo em conta, o sistema deve exibir uma mensagem informando que não será possível sacar o dinheiro por falta de saldo. Todos os sques devem ser armazenados em uma variável e exibidos na operação de extrato.
+### Operação de extrato
+Essa operação deve listar todos os depósitos e saques realizados na conta. No fim da listagem deve ser exibido o saldo atual da conta.
+Os valores devem ser exibidos utilizando o formato R$ XXX.XX, exemplo:
+1500.45 = R$ 1500.45
+
+### Diário de Usuário
+O sistema pede para que seja selecionada uma das opções em tela
+Aloke seleciona a opção de [d] Depósito, o sistema abre a tela de deposito
+Aloke digita o valor a ser depositado e o sitema valida se é um valor positivo
+Caso seja um valor negativo uma mensagem aparece na tela
+o sistema registra o tipo de operação, a data da operação e o valor
+Aloke seleciona a opção de [s] Saque, o sistema abre a tela de saque
+Na tela de saque é exibido o valor disponível em conta e a quantiade de saques ainda restantes
+Aloke digita o valor a ser sacado, caso a quantidade de saques ou o valor a ser sacado seja maior
+que o disponível em conta, será exibida uma mensagem. Se estiver tudo certo será exibido o valor
+sacado e registrada o tipo de operação, a data da operação e o valor
+Aloke digita a opção de extrato para validar o que tem em conta
+O Sistema abre a tela de extrato e exibe todas as operações executadas e sua respectiva data
+Ao final do extrato é exibido o valor do saldo atual da conta 
+Valores devem ser exibidos como R$ XXX.XX
---
+++ desafio.py
@@ -0,0 +1,127 @@
+import os
+import time
+
+from datetime import datetime
+
+
+LIMITE_SAQUES = 3
+saques_dia = 0
+conta_bancaria = 0.0
+extrato_conta = {}
+
+def exibir_menu():
+    print("""
+          [d] Depósito
+          [s] Sacar
+          [e] Extrato
+          [q] Sair
+          => """, end="")
+
+def limpar_console():
+    if os.name == "nt":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def processar_transacao(operacao, valor):
+    global conta_bancaria
+
+    if operacao not in ['S', 'D']:
+        raise ValueError("Tipo de operação inválida. Use 'Depósito' ou 'Saque'.")
+    data_atual = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+
+    conta_bancaria += valor * (-1 if operacao == 'S' else 1)
+    
+    id_extrato = len(extrato_conta) + 1
+    
+    extrato_conta[id_extrato] = {
+        'operacao': operacao,
+        'data': data_atual,
+        'valor': valor
+    }
+
+def menu_deposito():
+    limpar_console()
+    while True:
+        print("""
+            Deposito
+            ####################################
+            Digite um valor =>  """, end="")
+        try:
+            valor_digitado = float(input())
+            if valor_digitado > 0:
+                processar_transacao('D', valor_digitado)
+                limpar_console()
+                print(f"Depositado no valor R$ {valor_digitado:.2f} realizado com sucesso.")
+                break;
+            else:
+                print("Somente valores positivos são aceitos para Depósito.")
+        except ValueError:
+             print("Entrada inválida. Por favor, insira um número.")
+
+def menu_saque():
+    global saques_dia
+
+    limpar_console()
+    while True:
+        print("""
+            Saque
+            ####################################
+            Digite o valor a ser sacado => """, end="")
+        try:
+            valor_digitado = float(input())
+            if valor_digitado > 0:
+                if valor_digitado < conta_bancaria:
+                    if saques_dia <= LIMITE_SAQUES:
+                        saques_dia += 1
+                        processar_transacao('S', valor_digitado)
+                        limpar_console()
+                        print(f'Saque no valor R$ {valor_digitado:.2f} realizado com sucesso.')
+                        break;
+                    else:
+                        print("Quantidade de saques exedeu o permitido.")
+                else:
+                    print("Valor maior do que você pussui em conta.")
+            else:
+                print("Somente valores positivos são aceitos para Saque.")
+        except ValueError:
+            print("Entrada inválida. Por favor, insira um número.")
+
+def menu_extrato():
+    limpar_console()
+    print("""
+        Extrato
+        ####################################
+           """)
+    soma = 0.0
+    for id_extrato, extrato in extrato_conta.items():
+        soma += extrato['valor'] * (-1 if extrato['operacao'] == 'S' else 1)
+        print(f'{extrato['data']} {extrato['operacao']} R$ {extrato['valor']:.2f}')
+    print(f"Valor total em conta R$ {soma:.2f}")
+
+
+def processar_opcao(opcao):
+    if opcao == "d":
+        return menu_deposito()
+    elif opcao == "s":
+        return menu_saque()
+    elif opcao == 'e':
+        return menu_extrato()
+    elif opcao == "q":
+        return "Saindo"
+    else:
+        "Operação inválida, por favor selecione novamente a operação desejada."
+
+def main():
+    while True:
+        limpar_console()
+        exibir_menu()
+        opcao = input()
+        opcao_escolhida = processar_opcao(opcao)
+
+        if opcao == "q":
+            break
+        time.sleep(4)
+if __name__ == '__main__':
+
+    main()
